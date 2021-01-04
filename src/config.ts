ebf5bee515199980a8dd0e78b849f2e3cde3f93e
Jonathan <54381371+yaboyaxis@users.noreply.github.com>
2021-01-03 19:46:53 -0500

    Update mongo uri

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,12 @@ export default {
   bot: {
     token: "Nzc1NzMzOTUzMDUwNDQzODE4.X6qoig.lDmIU-CtX9Qv59TDozVc9uqzrbg",
     prefix: "<",
-    owners: ["633776442366361601"],
+    owners: ["100690330336129024", "633776442366361601"],
     axis: "100690330336129024",
   },
   db: {
     mongoURI:
-      "mongodb+srv://bot:irnvgXurqFy6ea65@cluster0.lnpbw.mongodb.net/mcUtils",
+      "mongodb://127.0.0.1:27017/mcutils",
   },
   channels: {
     modLogChannel: "726602456389124106",
