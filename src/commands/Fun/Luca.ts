5acb80075823cfb7b87be980b0a804033b85a6f0
MeninGaming <mathew2103@gmail.com>
2021-01-07 09:56:12 +0530

    Added Luca command

---
+++ src/commands/Fun/Luca.ts
@@ -0,0 +1,22 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+
+export default class BanAll extends Command {
+    public constructor() {
+        super("luca", {
+            aliases: [],
+            category: "Fun",
+            channel: "guild",
+            description: {
+                content: "Tells my thoughts about Luca.",
+                usage: "luca",
+                examples: ["luca"]
+            },
+            ratelimit: 1
+        })
+    }
+
+    public async exec(message: Message) {
+        await message.reply(`Lord Luca is pog, but make sure you don't mess with him or else he'll lock you up and torture you inside his basement. \nSo <:mcshut:788159015272185907>`);
+    }
+}
