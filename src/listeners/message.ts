70dcb323d7d5f8db621970c1b5ff9df370416259
Axis <54381371+yaboyaxis@users.noreply.github.com>
2021-01-10 20:00:10 -0500

    Added AFK Check and Link check for Automod

--- src/listeners/message.ts
+++ src/listeners/message.ts
@@ -1,8 +1,10 @@
 import { getModelForClass } from "@typegoose/typegoose";
 import { Listener } from "discord-akairo";
-import { Message, MessageEmbed } from "discord.js";
+import { Message } from "discord.js";
 import AfkModel from "../models/AfkModel";
-import { dispatchAfkEmbed } from "../structures/Utils";
+import AutoModModel from "../models/AutoModModel";
+import { dispatchAfkEmbed, dispatchAutoModMsg, autoModWarn } from "../structures/Utils";
+import urlRegexSafe from "url-regex-safe";
 
 export default class message extends Listener {
   public constructor() {
@@ -16,9 +18,19 @@ export default class message extends Listener {
   public async exec(message: Message) {
     await message.guild.members.fetch({ time: 20000 });
     const afkModel = getModelForClass(AfkModel);
-    const current_afk = await afkModel.findOne({ userId: message.author.id });
-    if (current_afk.afk.isAfk) {
-      return await dispatchAfkEmbed(message, current_afk.afk.status);
+    if (message.mentions.members.first()) {
+      const current_afk = await afkModel.findOne({ userId: message.mentions.members.first().id });
+      if (current_afk.afk.isAfk) {
+        return await dispatchAfkEmbed(message, current_afk.afk.status);
+      }
+    }
+    const autoModModel = getModelForClass(AutoModModel);
+    const current_automod = await autoModModel.findOne({ guildId: message.guild.id });
+    if (!current_automod.autoModSettings.exemptRoles.find((t) => message.member.roles.cache.findKey((r) => r.id === t))) {
+      if (message.content.match(urlRegexSafe({ strict: true }))) {
+        await autoModWarn(message.member, message.guild, "Sending Links", message, this.client);
+        await dispatchAutoModMsg("Sending Links", message, "Warned");
+      }
     }
   } 
 }
